b035f9b06093f73dac636c2b79a718cf3671e82c
Evelin <evelindacanovas@gmail.com>
2021-06-12 20:32:07 +0200

    doing more work after intitally publishing to Github

--- src/test/java/Practice1.java
+++ src/test/java/Practice1.java
@@ -5,5 +5,10 @@ public class Practice1 {
         System.out.println("EU5 is liking Git on IntelliJ");
 
         System.out.println("Work 1");
+
+        // successfully pushe projece to Github
+        //IntelliJ automatically create remot repo
+        //created connection betwee locar repo and remot repo
+        //pussehd locar comints to remot reop
     }
 }
